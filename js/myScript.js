e78be6ffbd31ef3d4526237a9c0a4a791f2132fa
SebiCF <55686742+SebiCF@users.noreply.github.com>
2019-10-12 01:08:39 +0200

    Finishes CodeRevoew03 bonus

--- js/myScript.js
+++ js/myScript.js
@@ -1,136 +1,108 @@
-var movies =
-
-    [
-    {
-
-        "name": "The Godfather",
-
-        "productionYear": "1972",
-
-        "image": "./img/godfather.jpg",
-
-        "description": "The aging patriarch of an organized crime dynasty transfers control of his clandestine empire to his reluctant son."
-
-    },
-    {
-
-        "name": "The Dark Knight",
-
-        "productionYear": "2008",
-
-        "image": "./img/darkknight.jpg",
-
-        "description": "When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice."
-
-    }, 
-    {
-
-        "name": "Pulp Fiction",
-
-        "productionYear": "1994",
-
-        "image": "./img/pulpfiction.jpg",
-
-        "description": "The lives of two mob hitmen, a boxer, a gangster and his wife, and a pair of diner bandits intertwine in four tales of violence and redemption."
 
 
-    },
-    {
-
-        "name": "Casablanca",
-
-        "productionYear": "1942",
-
-        "image": "./img/casablanca.jpg",
-
-        "description": "A cynical American expatriate struggles to decide whether or not he should help his former lover and her fugitive husband escape French Morocco."
-
-    },
-    {
-
-        "name": "Avengers: Endgame",
-
-        "productionYear": "2019",
-
-        "image": "./img/endgame.jpg",
-
-        "description": "After the devastating events of Avengers: Infinity War (2018), the universe is in ruins. With the help of remaining allies, the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe."
-
-    },
-    {
-
-        "name": "Inception",
-
-        "productionYear": "2010",
-
-        "image": "./img/inception.jpg",
-
-        "description": "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O."
-
-
-    },
-
-]
-
-function createHeader(){
-	let header = document.createElement("HEADER");
-	header.id = "header";
-	document.getElementById("mainSite").appendChild(header);
-
-	let heading = document.createElement("h1");
-	heading.innerHTML ="MovieFactory";
-	header.appendChild(heading);
-
-	let navbar = document.createElement("nav");
-	navbar.className = "navbar";
-
-	let ul = document.createElement("ul");
-	let li1 = document.createElement("li");
-	let li2 = document.createElement("li");
-	li1.innerHTML = `<a href="index.html" title="Home">Home</a>`;
-	li2.innerHTML = `<a href="index.html" title="Home">Movies</a>`;
-	header.appendChild(navbar);
-	navbar.appendChild(ul);
-	ul.appendChild(li1);
-	ul.appendChild(li2);
-
-}
-
-function creatMainContent(){
-	let main = document.createElement('main');
-	let contentHeader = document.createElement("div");
-	contentHeader.id = "contentHeader"
-	contentHeader.innerHTML =`<h2>Movies</h2><span>▲▼  Sort</span>`;
-
-	document.getElementById("mainSite").appendChild(main);
-	main.appendChild(contentHeader);
-
-}
-
-function createMovies(){
-	let movieFlexBox = document.createElement("div");
-	movieFlexBox.className ="flex";
-	document.querySelector("main").appendChild(movieFlexBox);
-
-	for(i=0; i < movies.length; i++){
-		let movieFlexChildren = document.createElement("div");
-		let movieImg = document.createElement("img");
-		let obj = movies[i];
-		movieImg.src = obj.image;
-		let likeBtnDiv = document.createElement("div");
-		likeBtnDiv.id = "likediv";
-		likeBtnDiv.innerHTML = `Like <img src="./img/thumbsup.png" id="likebutton">`;
-		let description = document.createElement("div");
-		description.innerHTML = `<h3>${obj.name}</h3>Year: ${obj.productionYear} <br><br> Synopsis:<br>  ${obj.description} `;
-		movieFlexBox.appendChild(movieFlexChildren);
-		movieFlexChildren.appendChild(movieImg);
-		movieFlexChildren.appendChild(description);
-		movieFlexChildren.appendChild(likeBtnDiv);
-
-	}
-}
+function initPage(){
 createHeader();
-creatMainContent();
-createMovies();
+createMain();
+createMovieFlexBox();
+createMovies(movies);
+increaseCounterOnClick(movies);
+}
+
+initPage();
+
+function createHeader() {
+
+    let header = document.createElement("HEADER");
+    header.id = "header";
+    document.getElementById("mainSite").appendChild(header);
+    let heading = document.createElement("h1");
+    heading.innerHTML = "MovieFactory";
+    header.appendChild(heading);
+    let navbar = document.createElement("nav");
+    navbar.className = "navbar";
+    let ul = document.createElement("ul");
+    let li1 = document.createElement("li");
+    let li2 = document.createElement("li");
+    li1.innerHTML = `<a title="Home">Home</a>`;
+    li2.innerHTML = `<a id="reloadDefaultMovies" title="Movies">Movies</a>`;
+    header.appendChild(navbar);
+    navbar.appendChild(ul);
+    ul.appendChild(li1);
+    ul.appendChild(li2);
+    document.getElementById("reloadDefaultMovies").addEventListener("click", function(){
+    	createMovies(movies);
+    });
+
+
+}
+
+function createMain() {
+    
+    let main = document.createElement('main');
+    let contentHeader = document.createElement("div");
+    contentHeader.id = "contentHeader"
+    contentHeader.innerHTML = `<h2>Movies</h2><span id="sortBtn">▲▼  Sort</span>`;
+	 document.getElementById("mainSite").appendChild(main);
+    main.appendChild(contentHeader);
+    document.getElementById("sortBtn").addEventListener("click", sortMoviesByLikes);
+
+}
+
+function createMovieFlexBox() {
+
+    let movieFlexBox = document.createElement("div");
+    movieFlexBox.className = "flex";
+    document.querySelector("main").appendChild(movieFlexBox);
+    }
+
+function createMovies(movies) {
+	document.querySelector(".flex").innerHTML = " ";
+    for (i = 0; i < movies.length; i++) {
+        let movieFlexChildren = document.createElement("div");
+        let movieImg = document.createElement("img");
+        movieImg.src = movies[i].image;
+        let likeBtnDiv = document.createElement("div");
+        likeBtnDiv.className = "likediv";
+        likeBtnDiv.innerHTML = `Like: <img src="./img/thumbsup.png" class="likebutton" id="likeBtn${i}"> <div id="likeAmount${i}" value="">${movies[i].likeCount}</div>`;
+        let description = document.createElement("div");
+        description.innerHTML = `<h3>${movies[i].name}</h3>Year: ${movies[i].productionYear} <br><br> Synopsis:<br>  ${movies[i].description} `;
+        document.querySelector(".flex").appendChild(movieFlexChildren);
+        movieFlexChildren.appendChild(movieImg);
+        movieFlexChildren.appendChild(description);
+        movieFlexChildren.appendChild(likeBtnDiv);
+    }
+}
+
+function increaseCounterOnClick(movies) {
+    let allLikeBtns = document.querySelectorAll(".likebutton")
+    allLikeBtns.forEach(function(elem) {
+            elem.addEventListener("click", function(e) {
+                for (i = 0; i < allLikeBtns.length; i++) {
+                    if (e.target.id == "likeBtn" + i) {
+                        let likeAmountId = "likeAmount" + i;
+                        let currentLikeCount = parseInt(movies[i].likeCount);
+                        movies[i].likeCount = currentLikeCount + 1;
+                        document.getElementById(likeAmountId).innerHTML = movies[i].likeCount;
+                    }
+                }
+            })
+        }
+
+
+
+    );
+}
+
+
+function sortMoviesByLikes() {
+    
+    let moviesByLikes = movies.slice(0);
+    moviesByLikes.sort(function(a, b) {
+        return b.likeCount - a.likeCount;
+    });
+    document.querySelector(".flex").innerHTML = " ";
+	createMovies(moviesByLikes);
+    increaseCounterOnClick(moviesByLikes);
+}
 
 
